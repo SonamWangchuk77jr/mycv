623286d4c1c1ea5ea26524ca3a9bbef6b14bd661
Sonam Wangchuk <sonamwangchuk77jr@gmail.com>
2024-12-26 16:40:49 +0600

    creating query builder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -6,10 +6,17 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/reports.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() getEstimateDto: GetEstimateDto) {
+    return this.reportService.getEstimateQuery(getEstimateDto);
+  }
+
   @Post()
   @UseGuards(AuthGuard)
   @Serialize(ReportDto)
